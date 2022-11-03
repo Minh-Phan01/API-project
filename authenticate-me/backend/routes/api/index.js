8e0cd9f01b027ca0888c249103624bb681604425
Minh Phan <minhphan@Minhs-MacBook-Pro.local>
2022-11-02 21:17:00 -0700

    merge with aut-setup

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+
+
+router.use(restoreUser);
+
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
-  
+
+
 
 module.exports = router;
